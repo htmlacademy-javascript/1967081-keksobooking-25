fbfd44e97c586ec9b9a9ee4825068be7d938f8ea
Chernavin Gleb <Cr4zyGleb@gmail.com>
2022-02-21 00:19:56 +0300

    edited2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 /* eslint-disable require-jsdoc */
 // https://learn.javascript.ru/task/random-min-max
 function getRandomIntMinMax(min, max) {
-  if (min < 0) {
-    min = 0;
-  }
-  if (max < 0) {
-    max = 0;
-  }
   if (min < max) {
     const swap = max;
     max = min;
@@ -18,24 +12,15 @@ function getRandomIntMinMax(min, max) {
 }
 getRandomIntMinMax(-1, 3);
 
-function getRandomFloatMinMax(min, max, decimalPlaces) {
-  if (min < 0) {
-    min = 0;
-  }
-  if (max < 0) {
-    max = 0;
-  }
+function getRandomFloatMinMax(min, max, digitsNumber) {
   if (min < max) {
     const swap = max;
     max = min;
     min = swap;
   }
 
-  if (typeof (decimalPlaces) == 'undefined') {
-    decimalPlaces = 2;
-  }
   let result = min + Math.random() * (max + 1 - min);
-  result = result.toFixed(decimalPlaces);
+  result = result.toFixed(digitsNumber);
   return result;
 }
 getRandomFloatMinMax(1, 3.5, 2);
