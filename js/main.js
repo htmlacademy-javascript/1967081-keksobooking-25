9f2e161b2a94112f50456c0c696834aaf430b49b
Chernavin Gleb <Cr4zyGleb@gmail.com>
2022-02-28 00:35:45 +0300

    task4:

--- js/main.js
+++ js/main.js
@@ -1,24 +1,89 @@
-// https://learn.javascript.ru/task/random-min-max
-function getRandomIntMinMax(min, max) {
-  if (min < max) {
-    const swap = max;
-    max = min;
-    min = swap;
-  }
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-  const rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+  return Math.floor(result);
 }
-getRandomIntMinMax(-1, 3);
 
-function getRandomFloatMinMax(min, max, digitsNumber) {
-  if (min < max) {
-    const swap = max;
-    max = min;
-    min = swap;
-  }
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+function getRandomPositiveFloat (a, b, digits = 2) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
 
-  const rand = min + Math.random() * (max + 1 - min);
-  return rand.toFixed(digitsNumber);
+  return +result.toFixed(digits);
 }
-getRandomFloatMinMax(1, 3.5, 2);
+
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const times = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const descriptions = ['Описание 1', 'Описание 2', 'Описание 3', 'Описание 4', 'Описание 5', 'Описание 6', 'Описание 7', 'Описание 8', 'Описание 9', 'Описание 10'];
+
+function getRandomFeatures(){
+  const randomFeatures = [];
+  for (let i = 1; i<=7; i++){
+    const index = getRandomPositiveInteger(0,5);
+    const elem = features[index];
+    if (!randomFeatures.includes(elem)){
+      randomFeatures.push(elem);
+    }
+  }
+  return randomFeatures;
+}
+
+function getPhotos(a, b){
+  const randomPhotos = [];
+  for (let i=a; i<=b; i++){
+    randomPhotos.push(`https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/.${i}jpg`);
+  }
+  return randomPhotos;
+}
+
+function createObjects(amount=10) {
+  const announcements = [];
+  for (let i=0; i<amount; i++){
+    const newAnnouncement = {};
+    //author
+    newAnnouncement.author = {};
+    const number = (i+1<10) ? `0${i+1}` : `${i+1}`;
+    //author.avatar
+    newAnnouncement.author.avatar = `img/avatars/user${number}.png`;
+    //offer
+    newAnnouncement.offer = {};
+    //offer.title
+    //возможно добавить выбор из структуры по индексу
+    newAnnouncement.offer.title = `Announcement №${i+1}`;
+    //location
+    newAnnouncement.location = {};
+    //location.lat
+    newAnnouncement.location.lat = getRandomPositiveFloat(35.65000,35.70000,5);
+    //location.lng
+    newAnnouncement.location.lng = getRandomPositiveFloat(139.70000,139.80000,5);
+    //price
+    newAnnouncement.price = getRandomPositiveInteger (1, 200000);
+    //type
+    newAnnouncement.type = types[getRandomPositiveInteger (0, 4)];
+    //rooms
+    newAnnouncement.rooms = getRandomPositiveInteger (1, 10);
+    //guests
+    newAnnouncement.guests = getRandomPositiveInteger (1, 30);
+    //checkin
+    newAnnouncement.checkin = times[getRandomPositiveInteger (0, 2)];
+    //checkout
+    newAnnouncement.checkout = times[getRandomPositiveInteger (0, 2)];
+    //features
+    newAnnouncement.features = getRandomFeatures();
+    //description
+    newAnnouncement.description = descriptions[i];
+    //photos
+    newAnnouncement.photos = getPhotos();
+    announcements.push(newAnnouncement);
+  }
+  return announcements;
+}
+
+createObjects(10);
