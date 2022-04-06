5555bbd222ad3900785c8df00b1d474b4774c09f
Chernavin Gleb <Cr4zyGleb@gmail.com>
2022-04-06 23:52:32 +0300

    Фото на память

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ import { initializeFormValidation } from './validationForms.js';
 import { initializateMap } from './map.js';
 import { initializatePriceSlider } from './slider.js';
 import { onUserEvents } from './userEvents.js';
+import { addImages } from './avatar.js';
+
 
 initializatePriceSlider();
 initializeFormValidation();
 initializateMap();
 onUserEvents();
+addImages();
 
